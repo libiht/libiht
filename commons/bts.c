dc26fe632daca962337ea81d6108a93080f64356
Thomason Zhao <thomasonzhao@outlook.com>
2023-12-29 11:24:49 +0000

    feat: basic code structure for bts finished, waiting for tests

--- commons/bts.c
+++ commons/bts.c
@@ -6,7 +6,7 @@
 //                   information.
 //
 //   Author        : Thomason Zhao
-//   Last Modified : Dec 15, 2023
+//   Last Modified : Dec 25, 2023
 //
 
 // Include Files
@@ -14,50 +14,307 @@
 
 //
 // Global Variables
+char bts_state_lock[MAX_LOCK_LEN];
+// Lock for bts state list
 
-void *bts_buffer;
-// BTS buffer
+char bts_state_head[MAX_LIST_LEN];
+// Head of bts state list
 
-
-
-// TODO
-s32 bts_init(void)
+void get_bts(struct bts_state *state)
 {
-    u64 ds_area, debugctlmsr;
+    u64 dbgctlmsr;
+
+    // Disable BTS
+    xrdmsr(MSR_IA32_DEBUGCTLMSR, &dbgctlmsr);
+    dbgctlmsr &= ~state->bts_request.bts_config;
+    xwrmsr(MSR_IA32_DEBUGCTLMSR, dbgctlmsr);
+
+    // Reset BTS debug store buffer pointer
+    xwrmsr(MSR_IA32_DS_AREA, NULL);
+}
+
+void put_bts(struct bts_state *state)
+{
+    u64 dbgctlmsr;
+
+    // Setup BTS debug store buffer pointer
+    xwrmsr(MSR_IA32_DS_AREA, state->ds_area);
+
+    // Enable BTS
+    xrdmsr(MSR_IA32_DEBUGCTLMSR, &dbgctlmsr);
+    dbgctlmsr |= state->bts_request.bts_config;
+    xwrmsr(MSR_IA32_DEBUGCTLMSR, dbgctlmsr);
+}
+
+void flush_bts()
+{
+    u64 dbgctlmsr;
+    u64 bts_bits;
+
+    bts_bits = DEBUGCTLMSR_TR |
+                DEBUGCTLMSR_BTS |
+                DEBUGCTLMSR_BTINT |
+                DEBUGCTLMSR_BTS_OFF_OS |
+                DEBUGCTLMSR_BTS_OFF_USR;
+
+    // Disable BTS
+    xprintdbg("LIBIHT-COM: Disable BTS on cpu core: %d...\n", xcoreid());
+    xrdmsr(MSR_IA32_DEBUGCTLMSR, &dbgctlmsr);
+    dbgctlmsr &= ~bts_bits;
+    xwrmsr(MSR_IA32_DEBUGCTLMSR, dbgctlmsr);
+
+    // Reset BTS debug store buffer pointer
+    xwrmsr(MSR_IA32_DS_AREA, NULL);
+}
+
+s32 enable_bts(struct bts_ioctl_request *request)
+{
+    struct bts_state *state;
+
+    state = find_bts_state(request->pid);
+    if (state != NULL)
+    {
+        xprintdbg("LIBIHT-COM: BTS already enabled for pid %d.\n",
+                    request->pid);
+        return -1;
+    }
+
+    state = create_bts_state();
+    if (state == NULL)
+    {
+        xprintdbg("LIBIHT-COM: Create BTS state failed.\n");
+        return -1;
+    }
+
+    // Setup fields for BTS state
+    state->bts_request.pid = request->pid ?
+                request->pid : xget_current_pid();
+    state->bts_request.bts_config = request->bts_config ? 
+                request->bts_config : DEFAULT_BTS_CONFIG;
+    state->bts_request.bts_buffer_size = request->bts_buffer_size ?
+                request->bts_buffer_size : DEFAULT_BTS_BUFFER_SIZE;
+    
+    // Setup fields for BTS debug store area
+    state->ds_area->bts_buffer_base = xmalloc(state->bts_request.bts_buffer_size);
+    state->ds_area->bts_index = 0;
+    state->ds_area->bts_absolute_maximum = 
+            state->ds_area->bts_buffer_base +
+            state->bts_request.bts_buffer_size + 1;
+    // Not yet support state->ds_area->bts_interrupt_threshold
+
+    insert_bts_state(state);
+}
+
+s32 disable_bts(struct bts_ioctl_request *request)
+{
+    struct bts_state *state;
+
+    state = find_bts_state(request->pid);
+    if (state == NULL)
+    {
+        xprintdbg("LIBIHT-COM: BTS not enabled for pid %d.\n",
+                    request->pid);
+        return -1;
+    }
+
+    remove_bts_state(state);
+    xfree(state->ds_area->bts_buffer_base);
+    xfree(state->ds_area);
+    xfree(state);
+}
+
+s32 dump_bts(struct bts_ioctl_request *request)
+{
+    struct bts_state *state;
+    struct bts_record *record;
+    u32 start, end;
+
+    state = find_bts_state(request->pid);
+    if (state == NULL)
+    {
+        xprintdbg("LIBIHT-COM: BTS not enabled for pid %d.\n",
+                    request->pid);
+        return -1;
+    }
+
+    // Dump some BTS buffer records
+    start = state->ds_area->bts_index - 32;
+    end = state->ds_area->bts_index;
+    for (u32 i = start; i < end; i++)
+    {
+        record = state->ds_area->bts_buffer_base + i;
+        xprintdbg("LIBIHT-COM: BTS record %d: from %llx to %llx.\n",
+                    i, record->from, record->to);
+    }
+}
+
+s32 config_bts(struct bts_ioctl_request *request)
+{
+    struct bts_state *state;
+
+    state = find_bts_state(request->pid);
+    if (state == NULL)
+    {
+        xprintdbg("LIBIHT-COM: BTS not enabled for pid %d.\n",
+                    request->pid);
+        return -1;
+    }
+
+    state->bts_request.bts_config = request->bts_config;
+    // If the current process is the target process, we need to
+    // disable and re-enable BTS to apply the new configuration
+    if (xgetcurrent_pid() == request->pid)
+    {
+        // TODO: check if it works
+        get_bts(state);
+
+        if (request->bts_buffer_size != state->bts_request.bts_buffer_size &&
+            request->bts_buffer_size != 0)
+        {
+            state->bts_request.bts_buffer_size = request->bts_buffer_size;
+
+            // Reconfigure BTS debug store area
+            xfree(state->ds_area->bts_buffer_base);
+            state->ds_area->bts_buffer_base = xmalloc(request->bts_buffer_size);
+            state->ds_area->bts_index = 0;
+            state->ds_area->bts_absolute_maximum = 
+                    state->ds_area->bts_buffer_base +
+                    request->bts_buffer_size + 1;
+        }
+
+        put_bts(state);
+    }
+}
+
+struct bts_state *create_bts_state()
+{
+    struct bts_state *state;
+    
+    state = xmalloc(sizeof(struct bts_state));
+    // TODO: check if this satisfy the page alignment requirement
+    state->ds_area = xmalloc(sizeof(struct ds_area));
+}
+
+struct bts_state *find_bts_state(u32 pid)
+{
+    void *curr_list;
+    struct bts_state *curr_state;
+    u32 offset;
+
+    // offsetof(st, m) macro implementation of stddef.h
+    offset = (u32)(&((struct bts_state *)0)->list);
+    curr_list = bts_state_head;
+    do
+    {
+        curr_list = xlist_next(curr_list);
+        if (curr_list == bts_state_head)
+            return NULL;
+        curr_state = (struct bts_state *)((u32)curr_list - offset);
+    } while (curr_state->bts_request.pid != pid);
+
+    return curr_state;
+}
+
+void insert_bts_state(struct bts_state *new_state)
+{
+    char irql_flag[MAX_IRQL_LEN];
+
+    if (new_state == NULL)
+        return;
+    
+    xacquire_lock(&bts_state_lock, irql_flag);
+    xlist_add(&new_state->list, &bts_state_head);
+    xprintdbg("LIBIHT-COM: Insert BTS state for pid %d.\n",
+                new_state->bts_request.pid);
+    xrelease_lock(&bts_state_lock, irql_flag);
+}
+
+void remove_bts_state(struct bts_state *old_state)
+{
+    char irql_flag[MAX_IRQL_LEN];
+
+    if (old_state == NULL)
+        return;
+    
+    xacquire_lock(&bts_state_lock, irql_flag);
+    xlist_del(&old_state->list);
+    xprintdbg("LIBIHT-COM: Remove BTS state for pid %d.\n",
+                old_state->bts_request.pid);
+    xrelease_lock(&bts_state_lock, irql_flag);
+}
+
+s32 bts_ioctl(struct xioctl_request *request)
+{
+    // TODO: Implement ioctl for BTS
+    s32 ret;
+
+    xprintdbg("LIBIHT-COM: BTS ioctl command %d.\n", request->cmd);
+    switch (request->cmd)
+    {
+    case LIBIHT_IOCTL_ENABLE_BTS:
+        xprintdbg("LIBIHT-COM: Enable BTS.\n");
+        ret = enable_bts(&request->data.bts);
+        break;
+    
+    case LIBIHT_IOCTL_DISABLE_BTS:
+        xprintdbg("LIBIHT-COM: Disable BTS.\n");
+        ret = disable_bts(&request->data.bts);
+        break;
+
+    case LIBIHT_IOCTL_DUMP_BTS:
+        xprintdbg("LIBIHT-COM: Dump BTS.\n");
+        ret = dump_bts(&request->data.bts);
+        break;
+
+    case LIBIHT_IOCTL_CONFIG_BTS:
+        xprintdbg("LIBIHT-COM: Config BTS.\n");
+        ret = config_bts(&request->data.bts);
+        break;
+    
+    default:
+        xprintdbg("LIBIHT-COM: Invalid BTS ioctl command.\n");
+        break;
+    }
+
+    return ret;
+}
+
+s32 bts_check(void)
+{
+    u32 edx;
+    u64 misc_msr;
 
     // Check if BTS is supported
-    xcpuid(0x7, NULL, NULL, NULL, &ds_area);
-    if (!(ds_area & (1UL << 25)))
+    xcpuid(0x1, 0x0, NULL, NULL, &edx);
+    if (!(edx & (1 << X86_FEATURE_DS)))
+        return -1;
+    
+    // Check if BTS is available
+    xrdmsr(MSR_IA32_MISC_ENABLE, &misc_msr);
+    if (!(misc_msr & MSR_IA32_MISC_ENABLE_BTS_UNAVAIL))
+        return -1;
+    
+    return 0;
+}
+
+s32 bts_init(void)
+{
+    // Check if BTS is supported and available
+    if (bts_check())
     {
-        xprintdbg("LIBIHT-COM: BTS is not supported\n");
+        xprintdbg("LIBIHT-COM: BTS is not supported or available.\n");
         return -1;
     }
 
-    // Check if BTS is enabled
-    xrdmsr(MSR_IA32_DEBUGCTLMSR, &debugctlmsr);
-    if (!(debugctlmsr & DEBUGCTLMSR_BTS))
-    {
-        xprintdbg("LIBIHT-COM: BTS is not enabled\n");
-        return -1;
-    }
-
-    // Initialize BTS
-    xwrmsr(MSR_IA32_DS_AREA, (u64)bts_buffer);
+    xinit_lock(&bts_state_lock);
+    xinit_list_head(&bts_state_head);
 
     return 0;
 }
 
-// TODO
 s32 bts_exit(void)
 {
-    u64 debugctlmsr;
-
-    // Disable BTS
-    xrdmsr(MSR_IA32_DEBUGCTLMSR, &debugctlmsr);
-    debugctlmsr &= ~DEBUGCTLMSR_BTS;
-    xwrmsr(MSR_IA32_DEBUGCTLMSR, debugctlmsr);
-
-    xwrmsr(MSR_IA32_DS_AREA, 0);
-
+    // Disable BTS on each cpu
+    xon_each_cpu(flush_bts);
     return 0;
 }
--- commons/bts.h
+++ commons/bts.h
@@ -6,15 +6,17 @@
 //  File           : commons/bts.h
 //  Description    : This is the header file for the BTS (Branch Trace Store)
 //                   module, which is used to capture the branch trace records
-//                   of a given process.
+//                   of a given process. Some of the macros and constants are
+//                   adapted from the Linux kernel source code.
 //
 //   Author        : Thomason Zhao
-//   Last Modified : Dec 15, 2023
+//   Last Modified : Dec 25, 2023
 //
 
 // Include Files
 #include "types.h"
 #include "xplat.h"
+#include "xioctl.h"
 
 // cpp cross compile handler
 #ifdef __cplusplus
@@ -23,6 +25,11 @@ extern "C" {
 
 //
 // Library constants
+
+// Intel-defined CPU features, CPUID level 0x00000001 (EDX), word 0
+#ifndef X86_FEATURE_DS
+#define X86_FEATURE_DS          (0*32+21)
+#endif
 
 // MSR related constants
 #ifndef MSR_IA32_DEBUGCTLMSR
@@ -33,6 +40,11 @@ extern "C" {
 #define MSR_IA32_DS_AREA        0x00000600
 #endif
 
+#ifndef MSR_IA32_MISC_ENABLE
+#define MSR_IA32_MISC_ENABLE    0x000001a0
+#endif
+
+// MSR bit shifts
 #ifndef DEBUGCTLMSR_TR
 #define DEBUGCTLMSR_TR          (1UL <<  6)
 #endif
@@ -51,6 +63,14 @@ extern "C" {
 
 #ifndef DEBUGCTLMSR_BTS_OFF_USR
 #define DEBUGCTLMSR_BTS_OFF_USR (1UL << 10)
+#endif
+
+#ifndef MSR_IA32_MISC_ENABLE_BTS_UNAVAIL_BIT
+#define MSR_IA32_MISC_ENABLE_BTS_UNAVAIL_BIT    11
+#endif
+
+#ifndef MSR_IA32_MISC_ENABLE_BTS_UNAVAIL
+#define MSR_IA32_MISC_ENABLE_BTS_UNAVAIL    (1ULL << MSR_IA32_MISC_ENABLE_BTS_UNAVAIL_BIT)
 #endif
 
 /* CPL-Qualified Branch Trace Store Encodings (Table 18-6 from Intel SDM)
@@ -75,6 +95,10 @@ extern "C" {
  *                                          buffer; generate an interrupt when 
  *                                          the buffer is nearly full
  */
+#define DEFAULT_BTS_CONFIG     (DEBUGCTLMSR_TR | DEBUGCTLMSR_BTS | DEBUGCTLMSR_BTS_OFF_OS)
+
+// BTS buffer size 0x200 * 2 = 0x400 = 1024 records
+#define DEFAULT_BTS_BUFFER_SIZE        (0x3000 << 1) 
 
 //
 // Type definitions
@@ -88,23 +112,36 @@ struct bts_record
     u64 misc;   // misc information
 };
 
-// Define Debug Store buffer management area
+// Define Debug Store buffer management area (Assuming 64-bit)
 struct ds_area
 {
     u64 bts_buffer_base;            // BTS buffer base
     u64 bts_index;                  // BTS current index
     u64 bts_absolute_maximum;       // BTS absolute maximum
     u64 bts_interrupt_threshold;    // BTS interrupt threshold
-    u64 pebs_buffer_base;
-    u64 pebs_index;
-    u64 pebs_absolute_maximum;
-    u64 pebs_interrupt_threshold;
+    u64 pebs_buffer_base;           // PEBS placeholder
+    u64 pebs_index;                 // PEBS placeholder
+    u64 pebs_absolute_maximum;      // PEBS placeholder
+    u64 pebs_interrupt_threshold;   // PEBS placeholder
 };
 
+// Define BTS state
+struct bts_state
+{
+    struct bts_ioctl_request bts_request;
+    struct ds_area *ds_area;        // Debug Store area pointer
+    struct bts_state *parent;       // Parent BTS state
+    char list[MAX_LIST_LEN];        // Kernel linked list
+};
 
 //
 // Global Variables
-// TODO: should be extern
+
+extern char bts_state_lock[MAX_LOCK_LEN];
+// The lock for bts_state_list.
+
+extern char bts_state_head[MAX_LIST_LEN];
+// The head of the bts_state_list.
 
 //
 // Function Prototypes
