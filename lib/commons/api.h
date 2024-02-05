dc573f22d90209c10350f09ca2e95f0955ea802d
suiseiseki-2016 <wd050602@gmail.com>
2024-02-05 22:27:14 +0800

    develop plugin

---
+++ lib/commons/api.h
@@ -0,0 +1,71 @@
+#include <unistd.h>
+
+#define MAX_LIST_LEN    0x20
+
+enum IOCTL{
+    LIBIHT_IOCTL_BASE,
+
+    LIBIHT_IOCTL_ENABLE_LBR,
+    LIBIHT_IOCTL_DISABLE_LBR,
+    LIBIHT_IOCTL_DUMP_LBR,
+    LIBIHT_IOCTL_SELECT_LBR,
+    LIBIHT_IOCTL_LBR_END,
+
+    LIBIHT_IOCTL_ENABLE_BTS,
+    LIBIHT_IOCTL_DISABLE_BTS,
+    LIBIHT_IOCTL_DUMP_BTS,
+    LIBIHT_IOCTL_CONFIG_BTS,
+    LIBIHT_IOCTL_BTS_END,
+};
+
+struct lbr_stack_entry{
+    unsigned long long from;
+    unsigned long long to;
+};
+
+struct lbr_config{
+    unsigned int pid;
+    unsigned long long lbr_select;
+};
+
+struct lbr_data{
+    unsigned long long lbr_tos;
+    struct lbr_stack_entry *entries;
+};
+
+struct lbr_ioctl_request{
+    struct lbr_config lbr_config;
+    struct lbr_data *buffer;
+};
+
+struct bts_config{
+    unsigned int pid;
+    unsigned long long bts_config;
+    unsigned long long bts_buffer_size;
+};
+
+struct bts_record{
+    unsigned long long from;
+    unsigned long long to;
+    unsigned long long misc;
+};
+
+struct bts_ioctl_request{
+    struct bts_config bts_config;
+    struct bts_data *buffer;
+};
+
+struct xioctl_request{
+    enum IOCTL cmd;
+    union{
+        struct lbr_ioctl_request lbr;
+        struct bts_ioctl_request bts;
+    }body;
+};
+
+// The above definitions are same as those in "types.h"
+
+struct lbr_ioctl_request enable_lbr();
+void disable_lbr(struct lbr_ioctl_request usr_request);
+void dump_lbr(struct lbr_ioctl_request usr_request);
+void select_lbr(struct lbr_ioctl_request usr_request);
---
+++ lib/lkm/src/libiht-gdb.py
@@ -0,0 +1,128 @@
+import os
+import gdb
+import ctypes
+
+# quote the ctypes
+
+class Clbr_stack_entry(ctypes.Structure):
+    _fields_ = [
+        ('from_', ctypes.c_ulonglong),
+        ('to', ctypes.c_ulonglong)
+    ]
+    def __init__(self, from_, to):
+        self.from_ = from_
+        self.to = to
+
+class Clbr_config(ctypes.Structure):
+    _fields_ = [
+        ('pid', ctypes.c_uint),
+        ('lbr_select', ctypes.c_ulonglong)
+    ]
+    def __init__(self, pid, lbr_select):
+        self.pid = pid
+        self.lbr_select = lbr_select
+
+class Clbr_data(ctypes.Structure):
+    _fields_ = [
+        ('lbr_tos', ctypes.c_ulonglong),
+        ('entries', ctypes.POINTER(Clbr_stack_entry))
+    ]
+    def __init__(self, lbr_tos, entries):
+        self.lbr_tos = lbr_tos
+        self.entries = entries
+
+class Clbr_ioctl_request(ctypes.Structure):
+    _fields_ = [
+        ('lbr_config', Clbr_config),
+        ('buffer', ctypes.POINTER(Clbr_data))
+    ]
+    def __init__(self, lbr_config, buffer):
+        self.lbr_config = lbr_config
+        self.buffer = buffer
+
+# declare the return value
+class LBRContent:
+    def __init__(self, from_, to):
+        self.from_ = from_
+        self.to = to
+
+# import the dynamic link
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+lib_path = os.path.join(script_dir, "liblbr_api.so")
+my_lib = ctypes.CDLL(lib_path)
+
+# get the funcions and define the arguments
+
+enable_lbr = my_lib.enable_lbr
+disable_lbr = my_lib.disable_lbr
+dump_lbr = my_lib.dump_lbr
+select_lbr = my_lib.select_lbr
+
+enable_lbr.restype = Clbr_ioctl_request
+enable_lbr.argtypes = [ctypes.c_uint]
+disable_lbr.argtypes = [Clbr_ioctl_request]
+dump_lbr.argtypes = [Clbr_ioctl_request]
+select_lbr.argtypes = [Clbr_ioctl_request]
+
+lbr_req = None
+lbr_enable = False
+
+def get_function_name(address):
+    symbol_output = gdb.execute("info symbol " + str(address), to_string=True)
+    lines = symbol_output.splitlines()
+    if len(lines) > 1:
+        function_name = lines[1]
+        function_name = function_name.split(' ', 1)[1]
+        return function_name
+    return address
+
+def get_gdb_pid():
+    inferior = gdb.selected_inferior()
+    if inferior is not None:
+        return inferior.pid
+    return None
+
+class EnableLBR(gdb.Command):
+    def __init__(self):
+        super(EnableLBR, self).__init__("enable_lbr", gdb.COMMAND_USER)
+
+    def invoke(self, args, from_tty):
+        global lbr_req
+        process_pid = get_gdb_pid()
+        lbr_req = enable_lbr(process_pid)
+        lbr_enable = True
+        print("LIBIHT-GDB: enable lbr for pid :", lbr_req.lbr_config.pid)
+
+class DisableLBR(gdb.Command):
+    def __init__(self):
+        super(DisableLBR, self).__init__("disable_lbr", gdb.COMMAND_USER)
+
+    def invoke(self, args, from_tty):
+        global lbr_req
+        disable_lbr(lbr_req)
+        lbr_enable = False
+        print("LIBIHT-GDB: disable lbr for pid :", lbr_req.lbr_config.pid)
+
+class DumpLBR(gdb.Command):
+    def __init__(self):
+        super(DumpLBR, self).__init__("dump_lbr", gdb.COMMAND_USER)
+    
+    def invoke(self, args, from_tty):
+        global lbr_req
+        print("LIBIHT-GDB: dump lbr for pid :", lbr_req.lbr_config.pid)
+        dump_lbr(lbr_req)
+        lbr_tos = lbr_req.buffer.contents.lbr_tos
+        data_pointer = ctypes.cast(lbr_req.buffer.contents.entries, ctypes.POINTER(Clbr_stack_entry))
+        lbr_content = []
+        for i in range(0, lbr_tos):
+            lbr_content.append(LBRContent(data_pointer[i].from_, data_pointer[i].to))
+        print(lbr_tos)
+        for i in range(0, lbr_tos):
+            print("MSR_LBR_NHM_FROM[", i, "]: ", get_function_name(hex(lbr_content[i].from_)))
+            print("MSR_LBR_NHM_TO  [", i, "]: ", get_function_name(hex(lbr_content[i].to)))
+        return lbr_content
+
+EnableLBR()
+DisableLBR()
+DumpLBR()
